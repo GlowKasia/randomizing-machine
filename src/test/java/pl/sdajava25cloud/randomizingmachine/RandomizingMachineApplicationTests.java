b6eb6dea6eb2334edbb1e3c397492052ae67ac40
GlowKasia <gwkasix@gmail.com>
2019-10-22 18:43:55 +0200

    config

---
+++ src/test/java/pl/sdajava25cloud/randomizingmachine/RandomizingMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdajava25cloud.randomizingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomizingMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
